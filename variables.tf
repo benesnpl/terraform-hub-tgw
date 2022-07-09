b053ec78ea8f221af6ec33f6d6efb834a7dfce6d
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-07-09 08:22:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,27 @@
-variable "aws_region" {
-	default = "us-east-1"
+#Variables For Connection
+
+
+variable "aws_access_key_id" {
+	default = "AKIA2I3LMA5XH4SEF5HD"
 }
+
+variable "aws_secret_access_key" {
+	default = "YVIXhSaBC7lodVrECzSTUBjodZ4SSEmQRqqhPlBR"
+}
+
+#Variables for general Account setup
+
+variable "coid" {
+	default = "BENE"
+}
+
+
+variable "coid" {
+	default = "BENE"
+}
+
+
+#VPC and subnets
 
 variable "create_vpc_shs" {
 	default = false
@@ -15,14 +36,30 @@ variable "vpcip_shs" {
 	default = "192.168.3.0/24"
 }
 
-variable "aws_access_key_id" {
-	default = "AKIA2I3LMA5XH4SEF5HD"
+variable "subnets_cidr_public" {
+	type = list
+	default = null
 }
 
-variable "aws_secret_access_key" {
-	default = "YVIXhSaBC7lodVrECzSTUBjodZ4SSEmQRqqhPlBR"
+variable "subnets_cidr_private" {
+	type = list
+	default = null
 }
 
-variable "coid" {
-	default = "BENE"
+/* Uncomment this part when you choose to deploy ShS VPC too
+
+variable "subnets_cidr_public_shr" {
+	type = list
+	default = null
 }
+
+variable "subnets_cidr_private_shr" {
+	type = list
+	default = null
+}
+
+*/
+	
+
+
+
