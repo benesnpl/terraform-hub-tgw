4e93bf9d8d9d99b119e125da0b85507f9d1356e0
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-07-09 09:10:26 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,43 +1,22 @@
-#Variables For Connection
-
-
-variable "aws_access_key_id" {
-	default = "AKIA2I3LMA5XH4SEF5HD"
+variable "aws_region" {
+	default = null
 }
 
-variable "aws_secret_access_key" {
-	default = "YVIXhSaBC7lodVrECzSTUBjodZ4SSEmQRqqhPlBR"
+variable "vpcip_fw" {
+	default = null
 }
 
-#Variables for general Account setup
+variable "vpcip_shr" {
+	default = null
+}
 
 variable "coid" {
-	default = "BENE"
-}
-
-variable "aws_region" {
 	default = null
 }
 
 variable "azs" {
 	type = list
 	default = null
-}
-
-
-#VPC and subnets
-
-variable "create_vpc_shs" {
-	default = false
-}
-
-
-variable "vpcip_fw" {
-	default = "192.168.2.0/24"
-}
-
-variable "vpcip_shs" {
-	default = "192.168.3.0/24"
 }
 
 variable "subnets_cidr_public" {
@@ -50,8 +29,6 @@ variable "subnets_cidr_private" {
 	default = null
 }
 
-/* Uncomment this part when you choose to deploy ShS VPC too
-
 variable "subnets_cidr_public_shr" {
 	type = list
 	default = null
@@ -62,8 +39,143 @@ variable "subnets_cidr_private_shr" {
 	default = null
 }
 
-*/
-	
 
 
+variable "rules_inbound_public_sg" {
+    default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["10.159.94.0/23","10.189.0.0/23"]
+    }
+    ]
+}
 
+variable "rules_outbound_public_sg" {
+   default = [
+	  {
+      port = 443
+      proto = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	  {
+      port = 80
+      proto = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	  {
+      port = 53
+      proto = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	{
+      port = 53
+      proto = "udp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	  {
+      port = 123
+      proto = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	  {
+      port = 123
+      proto = "udp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+    ]
+}
+
+variable "rules_inbound_public_sg_shr" {
+   default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["10.159.94.0/23","10.189.0.0/23"]
+    }
+    ]
+}
+
+variable "rules_outbound_public_sg_shr" {
+   default = [
+	  {
+      port = 443
+      proto = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	  {
+      port = 80
+      proto = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	  {
+      port = 53
+      proto = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	{
+      port = 53
+      proto = "udp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	  {
+      port = 123
+      proto = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+	  {
+      port = 123
+      proto = "udp"
+      cidr_block = ["0.0.0.0/0"]
+    },
+    ]
+}
+
+variable "rules_inbound_private_sg" {
+    default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["10.159.94.0/23","10.189.0.0/23"]
+    }
+    ]
+}
+
+variable "rules_inbound_private_sg_shr" {
+    default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["10.159.94.0/23","10.189.0.0/23"]
+    }
+    ]
+}
+
+variable "rules_outbound_private_sg" {
+    default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["10.159.94.0/23","10.189.0.0/23"]
+    }
+    ]
+}
+
+variable "rules_outbound_private_sg_shr" {
+    default = [
+    {
+      port = 0
+      proto = "-1"
+      cidr_block = ["10.159.94.0/23","10.189.0.0/23"]
+    }
+    ]
+}
+
+
+variable "il_external" {
+	default = null
+}
+
+variable "fl_external" {
+	default = null
+}
