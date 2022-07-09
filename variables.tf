a8934b2c2b9dd998356b9d58fd121eb8750ae633
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-07-09 06:36:34 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,11 @@ variable "eg_tgw_vpc_name" {
 variable "vpcip_tgw" {
 	default = "192.168.2.0/24"
 }
+
+variable "aws_access_key_id" {
+	default = null
+}
+
+variable "aws_secret_access_key" {
+	default = null
+}
