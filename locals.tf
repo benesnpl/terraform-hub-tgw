a8c154773c7dbe8d518e026ed2edfcee4a8d45ac
Benelab <78307945+benesnpl@users.noreply.github.com>
2022-07-09 06:49:39 -0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-   tgw_vpc_id                          = "${var.create_vpc_tgw ? aws_vpc.tgw_vpc[count.index].id : data.aws_vpc.tgw_vpc[count.index].id}"
+   tgw_vpc_id                          = "${var.create_vpc_tgw ? aws_vpc.tgw_vpc.*.id : data.aws_vpc.tgw_vpc.*.id}"
 }
